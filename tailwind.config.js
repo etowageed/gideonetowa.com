165393d12c3cca69466537f7fd043bef8700a92a
Gideon Etowa <etowagideon@gmailcom>
2021-11-26 14:16:07 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'dark-text': '#3d3d3d',
+        'primary-accent': '#9490FA',
+        'primary-col': '#5F59F7',
+        'primary-txt': '#9490FA',
+        'input': '#2E2E35',
+      },
+      fontFamily: {
+        'lato': ["'lato'", 'sans-serif']
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+}
