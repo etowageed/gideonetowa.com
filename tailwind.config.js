b82d952fa299af9778ffcf17c7d1f5a129c057b3
etowageed <etowagideon@gmail.com>
2022-12-20 23:58:10 +0100

    refactored the files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,34 @@
 module.exports = {
-  content: ['./public/**/*.html',
-            './public/**/*.js'],
+  content: ["./**/*.{html, js}"],
   theme: {
     extend: {
       zIndex: {
-'100' : 100,
+        100: 100,
       },
       colors: {
-        'body': '#17171F',
-        'dark-text': '#3d3d3d',
-        'primary-accent': '#9490FA',
-        'primary-col': '#5F59F7',
-        'primary-txt': '#9490FA',
-        'input': '#2E2E35',
+        body: "#17171F",
+        "dark-text": "#3d3d3d",
+        "primary-accent": "#9490FA",
+        "primary-col": "#5F59F7",
+        "primary-txt": "#9490FA",
+        input: "#2E2E35",
       },
       fontFamily: {
-        'lato': ["'lato'", 'sans-serif']
+        lato: ["'lato'", "sans-serif"],
       },
       width: {
-        '600px': '37.5rem',
-        '500px': '31.25rem',
-        '48%': '48%',
-        '45%': '45%',
+        "600px": "37.5rem",
+        "500px": "31.25rem",
+        "48%": "48%",
+        "45%": "45%",
       },
       height: {
-        '420px': '420px',
-        '22rem':'22rem',
+        "420px": "420px",
+        "22rem": "22rem",
       },
     },
   },
   variants: {
     extend: {},
   },
-}
+};
